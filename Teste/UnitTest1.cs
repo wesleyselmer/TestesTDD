370af43f6a72e66493b90747dba94b9dea0dc262
Wesley Selmer <wesleyselmer@gmail.com>
2024-08-23 10:15:17 -0300

    Adicionar arquivos de projeto.

---
+++ Teste/UnitTest1.cs
@@ -0,0 +1,78 @@
+using System;
+using TestesTDD;
+using Xunit;
+
+namespace Teste
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData (1, 2, 3)]
+        [InlineData(4, 5, 9)]
+        public void TesteSomar(int val1, int val2, int resultado)
+        {
+            Calculadora calc = new Calculadora();
+            int resultadoCalculadora = calc.Somar(val1, val2);
+
+            Assert.Equal(resultado, resultadoCalculadora);
+        }
+
+        [Theory]
+        [InlineData(2, 1, 1)]
+        [InlineData(5, 2, 3)]
+        public void TesteSubtrair(int val1, int val2, int resultado)
+        { 
+            Calculadora calc = new Calculadora();
+            int resultadoCalculadora = calc.Subtrair(val1, val2);
+
+            Assert.Equal(resultado, resultadoCalculadora);
+        }
+
+        [Theory]
+        [InlineData(1, 2, 2)]
+        [InlineData(4, 5, 20)]
+        public void TesteMultiplicar(int val1, int val2, int resultado)
+        {
+            Calculadora calc = new Calculadora();
+            int resultadoCalculadora = calc.Multiplicar(val1, val2);
+
+            Assert.Equal(resultado, resultadoCalculadora);
+        }
+
+        [Theory]
+        [InlineData(20, 2, 10)]
+        [InlineData(40, 5, 8)]
+        public void TesteDividir(int val1, int val2, int resultado)
+        {
+            Calculadora calc = new Calculadora();
+            int resultadoCalculadora = calc.Dividir(val1, val2);
+
+            Assert.Equal(resultado, resultadoCalculadora);
+        }
+
+        [Fact]
+        public void TestarDivisaoPorZero()
+        {
+            Calculadora calc = new Calculadora();
+            Assert.Throws<DivideByZeroException>(
+                () => calc.Dividir(3, 0)
+                );
+        }
+
+        [Fact]
+        public void TestarHistorico()
+        {
+            Calculadora calc = new Calculadora();
+
+            calc.Somar(1, 2);
+            calc.Somar(2, 2);
+            calc.Somar(3, 2);
+            calc.Somar(4, 2);
+
+            var lista = calc.Historico();
+
+            Assert.NotEmpty(lista);
+            Assert.Equal(3, lista.Count);
+        }
+    }
+}
---
+++ TestesTDD/Calculadora.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestesTDD
+{
+    public class Calculadora
+    {
+        private List<string> listaHistorico;
+
+        public Calculadora()
+        {
+            listaHistorico = new List<string>();
+        }
+
+        public int Somar(int val1, int val2)
+        {
+            int res = val1 + val2;
+            listaHistorico.Insert(0, "Res: " + res);
+            return res;
+        }
+        public int Subtrair(int val1, int val2)
+        {
+            int res = val1 - val2;
+            listaHistorico.Insert(0, "Res: " + res);
+            return res;
+        }
+        public int Multiplicar(int val1, int val2)
+        {
+            int res = val1 * val2;
+            listaHistorico.Insert(0, "Res: " + res);
+            return res;
+        }
+        public int Dividir(int val1, int val2)
+        {
+            int res = val1 / val2;
+            listaHistorico.Insert(0, "Res: " + res);
+            return res;
+        }
+
+        public List<string> Historico()
+        {
+            listaHistorico.RemoveRange(3, listaHistorico.Count-3);
+            return listaHistorico;
+        }
+    }
+}
